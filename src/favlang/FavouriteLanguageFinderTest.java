89d2b6684a193a763db9ac143a879233a88184c1
Oliver Norton <onorton125@gmail.com>
2016-11-16 11:58:26 +0000

    Added more tests and reduced abstraction for better tests
    Signed-off-by: Oliver Norton <onorton125@gmail.com>

--- src/favlang/FavouriteLanguageFinderTest.java
+++ src/favlang/FavouriteLanguageFinderTest.java
@@ -7,17 +7,41 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.fail;
-
 /**
  * Created by Ollie on 29/02/2016.
  */
 public class FavouriteLanguageFinderTest {
 
+    private Map<String, Integer> languageCounts = new HashMap<>();
+
     @Test
     public void GettingFavouriteLanguageEmptyListReturnsNull() {
-        Map<String, Integer> languageCounts = new HashMap<>();
         Assert.assertNull(FavouriteLanguageFinder.getFavouriteLanguage(languageCounts));
+    }
+
+    @Test
+    public void GettingFavouriteLanguageSingleItemReturnsLanguage() {
+        languageCounts.put("java", 2);
+        Assert.assertEquals("java", FavouriteLanguageFinder.getFavouriteLanguage(languageCounts));
+    }
+
+    @Test
+    public void GettingFavouriteLanguageMultipleReturnsHighestCount() {
+        languageCounts.put("java", 10);
+        languageCounts.put("C++", 2);
+        Assert.assertEquals("java", FavouriteLanguageFinder.getFavouriteLanguage(languageCounts));
+    }
+
+    @Test
+    public void GettingFavouriteLanguageMultipleHighestReturnsOneWithHighest() {
+        languageCounts.put("C", 10);
+        languageCounts.put("Java", 10);
+        languageCounts.put("Ruby", 10);
+        languageCounts.put("C++", 2);
+        String favLang = FavouriteLanguageFinder.getFavouriteLanguage(languageCounts);
+        int highestCount = languageCounts.get(favLang);
+        Assert.assertEquals(10, highestCount);
+        Assert.assertTrue(favLang.equals("C") || favLang.equals("Java") || favLang.equals("Ruby"));
     }
 
     @Test(expected = IOException.class)
